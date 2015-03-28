ede2b1adddc440185b0255e571c679a4e4dc30c9
murphyky <kyle-murphy89@live.com>
2015-03-28 11:32:58 +0000

    backing up

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var fortune = require('./lib/fortune.js');
 
 var app = express();
 
-var handlebars = require('express3-handlebars')
+var handlebars = require('express-handlebars')
 	.create({defaultLayout:'main'});
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
@@ -12,13 +12,35 @@ app.set('port', process.env.PORT || 3000);
 
 app.use(express.static(__dirname + '/public'));
 
+app.use(function(req, res, next) {
+	res.locals.showTests = app.get('env') !== 'production' 
+	&& req.query.test === '1';
+	next();
+});
+
+app.get('/headers', function(req, res){
+	res.set('Content-Type', 'text/plain');
+	var s = '';
+	for(var name in req.headers) s += name + ': ' + req.headers[name] + '\n';
+		res.send(s);
+});
+
 app.get('/', function(req, res) {
 	res.render('home');
 });
 app.get('/about', function(req, res) {
 	res.render('about', {
-		fortune: fortune.getFortune();
+		fortune: fortune.getFortune(),
+		pageTestScript: '/qa/tests-about.js'
 	});
+});
+
+app.get('/tours/hood-river', function(req, res){
+	res.render('tours/hood-river');
+});
+
+app.get('/tours/request-group-rate', function(req, res) {
+	res.render('tours/request-group-rate');
 });
 
 app.use(function(req, res, next) {
